5c85bd7dbe99eb3d1eaba30921dbd5890b43ca2a
Pier Bover <pierbover11@gmail.com>
2020-12-03 22:36:05 -0600

    Update FaunaError.js

--- errors/FaunaError.js
+++ errors/FaunaError.js
@@ -2,40 +2,31 @@ class FaunaError extends Error {
   constructor (error) {
     super();
 
-    const {name, message} = error;
+    const errors = error.requestResult.responseContent.errors;
 
-    this.name = name;
-    this.message = message;
-    this.status = 500;
+    this.code = errors[0].code;
+    this.message = errors[0].description;
+    this.statusCode = 500;
 
-    if (name === 'BadRequest' && message === 'instance not unique'){
-      this.status = 409;
+    if (this.code === 'instance not unique'){
+      this.statusCode = 409;
     }
 
-    if (name === 'BadRequest' && message === 'authentication failed') {
-      this.status = 401;
+    if (this.code === 'authentication failed') {
+      this.statusCode = 401;
     }
 
-    if (name === 'Unauthorized') {
-      this.status = 401;
+    if (this.code === 'unauthorized') {
+      this.statusCode = 401;
     }
 
-    if (name === 'NotFound') {
-      this.status = 404;
+    if (this.code === 'instance not found') {
+      this.statusCode = 404;
     }
 
-    if (name === 'PermissionDenied') {
-      this.status = 403;
+    if (this.code === 'permission denied') {
+      this.statusCode = 403;
     }
-  }
-
-  toString () {
-    // We're following Fastify's convention so that all server errors look the same
-    return JSON.stringify({
-      error: this.name,
-      message: this.message,
-      statusCode: this.status
-    });
   }
 }
 
